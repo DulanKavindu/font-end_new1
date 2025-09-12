819cd56f2c002d2b6e7fe7014cc26ef8567b9539
DulanKavindu <163100627+DulanKavindu@users.noreply.github.com>
2025-09-13 02:24:08 +0530

    add react icons

--- src/pages/addminpage.jsx
+++ src/pages/addminpage.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
+import { BsGraphUp } from "react-icons/bs";
 
 export function Addminpage(){
     return(
         <div className="bg-blue-300 w-full h-screen flex">
         <div className="bg-blue-200 w-[20%] h-[100%]  flex flex-col  items-center gap-30 pt-20">  
-            <Link to='/addmin/dashboard' > Dashbord </Link>
+            <Link to='/addmin/dashboard' > Dashbord <BsGraphUp /> </Link>
             <Link to='/addmin/products' > Products </Link>
             <Link to='/addmin/order' > Users </Link>
             <Link to='/addmin/customer' > Settings </Link>
